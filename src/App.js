3097b94f971eb9c567b6c018eb29cc25efacec1b
herigb <heriberto@dojiggy.com>
2020-07-09 18:45:31 -0500

    testing pipeline
    testing pipeline

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <h1>Hello</h1>
+          <h1>Hello, React</h1>
           To get started, edit <code>src/App.js</code> and save to reload. 
         </p>
       </div>
